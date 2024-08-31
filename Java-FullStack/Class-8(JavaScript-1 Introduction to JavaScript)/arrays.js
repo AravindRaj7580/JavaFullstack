7d33e2a14a9de2530eac057165fc6d7b081a5c99
Aravindraj R <raravindraj7590@gmail.com>
2024-08-31 16:49:02 +0530

    array methods injavascripdt class-1

---
+++ Java-FullStack/Class-8(JavaScript-1 Introduction to JavaScript)/arrays.js
@@ -0,0 +1,45 @@
+let arr = [1,'Scaler',true,6, undefined, [1,2,3,4,5]]
+
+console.log(arr)
+
+//how to access element in a array
+
+console.log(arr[1])
+
+//reassing an array element
+
+// arr[2] = 'mrinal'
+
+// console.log(arr[2])
+
+let cars = ['Urus', 'Bentley', 'Audi']
+
+//push method
+
+// cars.push('BMW')
+
+// console.log(cars)
+
+//pop method
+
+// console.log(cars.pop())
+
+// console.log(cars.pop())
+
+console.log(cars)
+
+//Shift and unshift
+
+//unshift
+
+//will add an element at the start of your array
+
+cars.unshift('ferrari')
+
+console.log(cars)
+
+//shift will remove first element
+
+cars.shift()
+
+console.log(cars)
---
+++ Java-FullStack/Class-8(JavaScript-1 Introduction to JavaScript)/variables.js
@@ -0,0 +1,57 @@
+'use strict'
+// var a = 2
+
+// console.log(a)
+
+// var b = "Aravind is awesome"
+
+// console.log(b)
+
+// b = 6
+
+// console.log(b)
+
+// var c = true
+
+// console.log(c)
+
+// var d = null
+
+// d= "hi"
+
+// console.log(d)
+
+// var e = undefined
+
+// console.log(e)
+
+
+// var f = undefined
+
+// var name1 = 'Narsing'
+
+// var name1 = 'aravind'
+
+// console.log(name1)
+
+
+// let name2 = 'verma'
+
+// let name2 = 'verma2'
+
+
+let name2 = 'verma'
+
+name2 = 'verma2'
+
+console.log(name2)
+
+const a = 40
+
+// a = 60
+
+// y = 56
+
+// console.log(y)
+
+console.log(a)
